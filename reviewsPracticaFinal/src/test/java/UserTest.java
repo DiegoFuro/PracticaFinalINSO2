69656e5101875fbd26c1e5425ad0637c84bf2350
asant05 <55275643+asant05@users.noreply.github.com>
2023-06-02 20:38:57 +0200

    pruebas

---
+++ reviewsPracticaFinal/src/test/java/UserTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Controller.registerController;
+import Modelo.Rol;
+import Modelo.Usuario;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Diego
+ */
+public class UserTest {
+
+    private static Usuario user;
+    private static Usuario user2;
+
+    private static Rol rol;
+    private registerController registerController;
+    private registerController registerController1;
+    private registerController registerController2;
+    private registerController registerController3;
+    
+    @BeforeAll
+    public void setUp() throws Exception {
+        registerController = new registerController();
+        registerController1 = new registerController();
+        registerController2 = new registerController();
+        registerController3 = new registerController();
+
+    }
+    
+    @Test
+    public void testNombreValidoSinNumeros() {
+        registerController.setName("Juan"); 
+        registerController.setLastName("Rodriguez");
+        registerController.setUser("juanRod");
+        registerController.setPassword("password");
+        registerController.setIdRol(3);
+        String resultado = registerController.registerUser();
+        assertTrue(resultado.equals("index.xhtml?faces-redirect=true"));
+    }
+
+    @Test
+    public void testNombreNoValidoConNumeros() {
+        registerController1.setName("Juan123");
+        registerController1.setLastName("Rodriguez");
+        registerController1.setUser("juanRod");
+        registerController1.setPassword("password");
+        registerController1.setIdRol(3);
+        String resultado = registerController1.registerUser();
+        assertTrue(resultado.equals(""));
+    }
+    
+    @Test
+    public void testUserValido() {
+        registerController2.setName("Juan"); 
+        registerController2.setLastName("Rodriguez");
+        registerController2.setUser("juanRod");
+        registerController2.setPassword("password");
+        registerController2.setIdRol(3);
+        String resultado = registerController2.registerUser();
+        assertTrue(resultado.equals("index.xhtml?faces-redirect=true"));
+    }
+    
+    @Test
+    public void testUserNoValido() {
+        registerController3.setName("Juan"); 
+        registerController3.setLastName("Rodriguez");
+        registerController3.setUser("juanRod$");
+        registerController3.setPassword("password");
+        registerController3.setIdRol(3);
+        String resultado = registerController3.registerUser();
+        assertTrue(resultado.equals("index.xhtml?faces-redirect=true"));
+    }
+}
